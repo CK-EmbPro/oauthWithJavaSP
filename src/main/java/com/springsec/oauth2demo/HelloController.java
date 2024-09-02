5c8537c678b12d8de69ca6031ca556253e4be541
CK-EmbPro <kennydebrice2@gmail.com>
2024-09-02 12:26:03 +0200

    oauth with google

---
+++ src/main/java/com/springsec/oauth2demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.springsec.oauth2demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to oauth protocol using google";
+    }
+}
